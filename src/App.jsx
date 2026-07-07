4bae403a3188bed7dd8ffc4be5e6e92c325d276a
Gaju Sandra <gajusandra872@gmail.com>
2026-07-07 15:53:19 +0200

    the total of l and r BUT THEN giving total-1

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,39 @@
 import { useState } from 'react'
 
-const Display = ({ counter }) => <div>{counter}</div>
-
-const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>
-
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
+  const [allClicks, setAll] = useState([])
 
-  console.log('rendering with counter value', counter)
+  const [total, setTotal] = useState(0)
 
-  const increaseByOne = () => {
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
 
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+    setTotal(left + right)
   }
 
-  const decreaseByOne = () => { 
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+    setTotal(left + right)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      {left}
+      <button onClick={handleLeftClick}>left</button>
+      <button onClick={handleRightClick}>right</button>
+      {right}
+      <p>{allClicks.join(' ')}</p>
+
+      <p>total {total}</p>
     </div>
   )
-} 
+}
+
+
+
 export default App
