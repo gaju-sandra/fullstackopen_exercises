2977d23d885940e75bdfb3c0f3e8310164231220
Gaju Sandra <gajusandra872@gmail.com>
2026-07-07 12:30:53 +0200

    exercise part1 b

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,17 @@
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
-
-  return (
-    <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
+class Person {
+  constructor(name, age) {
+    this.name = name
+    this.age = age
+  }
+  greet() {
+    console.log('hello, my name is ' + this.name)
+  }
 }
+
+const adam = new Person('Adam Ondra', 33)
+adam.greet()
+
+const janja = new Person('Janja Garnbret', 27)
+janja.greet()
 
 export default App
