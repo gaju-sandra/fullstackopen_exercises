25fce70b2da1b377c5a13d68c0261c37d78ae46c
Gaju Sandra <gajusandra872@gmail.com>
2026-07-07 15:28:07 +0200

    three buttons,plus, zero, minus

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { useState } from 'react'
-
 const App = () => {
   const [ counter, setCounter ] = useState(0)
+
+
+  const increaseByOne = () => setCounter(counter + 1)
+  const decreaseByOne = () => setCounter(counter - 1)
+  const setToZero = () => setCounter(0)
 
   return (
     <div>
       <div>{counter}</div>
-      <button onClick={() => setCounter(counter + 1)}>
+
+      <button onClick={increaseByOne}>
         plus
       </button>
 
-      <button onClick={() => setCounter(2)}> 
+      <button onClick={setToZero}>
         zero
+      </button>
+      <button onClick={decreaseByOne}>
+        minus
       </button>
     </div>
   )
